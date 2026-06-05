e04106368f21efa292c0487cd6a4e68aee74fc33
JagadeeshPython <jagadeeshb0306@gmail.com>
2026-06-05 15:15:07 +0530

    Add STATIC_ROOT for deployment

--- hostelmanagementsystem/settings.py
+++ hostelmanagementsystem/settings.py
@@ -119,10 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
