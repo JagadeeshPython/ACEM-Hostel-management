bf62f730209980a23c41cd434fc91469c8c212cb
JagadeeshPython <jagadeeshb0306@gmail.com>
2026-06-05 15:03:45 +0530

    Prepare project for deployment

--- hostelmanagementsystem/settings.py
+++ hostelmanagementsystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zmfn^3%hu=ht6t#+04xa6c(z+rtx1i0*uamqoqb$pg8@_8y$1u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hostelmanagementsystem.urls'
